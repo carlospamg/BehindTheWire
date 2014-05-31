316960436985149cf822d7465aefd3f87869d71f
Carlos Pereira Atencio <carlospa87@gmail.com>
2014-05-31 22:06:26 +0100

    Removed "Run" button, minor updates to js.

--- blockly/apps/loadsketch/ArduAplet.js
+++ blockly/apps/loadsketch/ArduAplet.js
@@ -1,14 +1,12 @@
-function sendSketchToApplet() {
-   var myApp = document.applets['ArduApplet'];
-   myApp.processSketch(document.getElementById("sketchTextArea").value);
-   document.getElementById("sketchTextArea").value =  'TEST' + document.getElementById("sketchTextArea").value;
-}
-
-function getSketchToApplet() {
+function getSketchString() {
    return Blockly.Generator.workspaceToCode('Arduino');
 }
 
 function displaySketchInAlert() {
-   var mySketch = Blockly.Generator.workspaceToCode('Arduino');
-alert(mySketch);
+   alert(getSketchString());
+}
+
+function sendSketchToApplet() {
+   var myApp = document.applets['ArduApplet'];
+   myApp.processSketch(getSketchString());
 }
