1ec682ce45b20d319eb73c23b02fcb774dbd2c2e
DanseMacabre <Danse@jarofdoom.co.uk>
2014-06-02 10:08:44 +0100

    Update btw.js
    No input required, data selected in dropdown.

--- blockly/generators/arduino/btw.js
+++ blockly/generators/arduino/btw.js
@@ -253,7 +253,7 @@ Blockly.Language.wait = {
 	helpUrl: '',
 	init: function() {
 		this.setColour(230);
-		this.appendValueInput("WAIT_AMOUNT",Number)
+		this.appenddummyInput("")
 			.appendTitle("Wait ")
 			.appendTitle(new Blockly.FieldDropdown([["0.5",500],["1",1000],["1.5",1500],["2",2000]]),"WAIT")
 			.appendTitle(" seconds");
