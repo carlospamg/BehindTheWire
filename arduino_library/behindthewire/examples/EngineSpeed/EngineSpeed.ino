e65992d496d1a37c42622e3278d4909c3fc935df
Carlos Pereira Atencio <carlospa87@gmail.com>
2014-06-02 23:29:11 +0100

    Rever rudder direction. Small fixes to examples.

--- arduino_library/behindthewire/examples/EngineSpeed/EngineSpeed.ino
+++ arduino_library/behindthewire/examples/EngineSpeed/EngineSpeed.ino
@@ -6,20 +6,22 @@ void setup() {
 
   pinMode(EngineLeft,OUTPUT);
   pinMode(EngineRight,OUTPUT);
-  
+  pinMode(RedLight,OUTPUT);
+
 }
 
 void loop() {
 
-   power = map(analogRead(Throttle),0,1024,0,255);
-   analogWrite(EngineLeft, power);
-   analogWrite(EngineRight, power);
+  power = map(analogRead(Throttle),0,1024,0,150);
+  analogWrite(EngineLeft, power);
+  analogWrite(EngineRight, power);
 
-   if (power > 200) {
-      digitalWrite(RedLight,ON);
-   }
-   else {
-      digitalWrite(RedLight,OFF);
-   }
+  if (power > 120) {
+    digitalWrite(RedLight,ON);
+  }
+  else {
+    digitalWrite(RedLight,OFF);
+  }
+  delay(100);
 
 }
--- arduino_library/behindthewire/examples/RudderControl/RudderControl.ino
+++ arduino_library/behindthewire/examples/RudderControl/RudderControl.ino
@@ -8,16 +8,16 @@ int ButtonRight = 0;
 
 void setup() {
 
-  pinMode(LeftButton,INPUT);         // Make the Up Button an Input
-  pinMode(RightButton,INPUT);        // Make the Down Button an Input
-  functions.rudderPrepare();         // Set up the landing gear
+  pinMode(LeftButton,INPUT);         // Make the Left Button an Input
+  pinMode(RightButton,INPUT);        // Make the Right Button an Input
+  functions.rudderPrepare();         // Set up the rudder in position
 
 }
 
 void loop() {
 
-  ButtonLeft = digitalRead(LeftButton);     // Get the state of Up Button
-  ButtonRight = digitalRead(RightButton);   // Get the state of Down Button
+  ButtonLeft = digitalRead(LeftButton);     // Get the state of Left Button
+  ButtonRight = digitalRead(RightButton);   // Get the state of Right Button
 
   if(ButtonLeft == PRESSED) {
     functions.rudderLeft();
